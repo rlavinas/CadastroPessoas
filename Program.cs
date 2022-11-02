8e818b7c86eabbaa592eda343ecf7a76067ba395
rafaellaivnas <rafael.lavinas@korusconsultoria.com.br>
2022-11-02 13:25:37 -0300

    FEAT: Atualização Documentação e Melho. Cadastro

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using EncontroRemoto;
+﻿
+using EncontroRemoto;
+using System.Threading;
 
 namespace EncontroRemoto
 {
@@ -6,54 +8,118 @@ namespace EncontroRemoto
     {
         static void Main(string[] args)
         {
-            Endereco endPF = new Endereco();
-            endPF.Logradouro  = "Rua Silveira Martins";
-            endPF.Numero      = 24;
-            endPF.Complemento = "Apto 113";
-            endPF.EnderecoComercial = false;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(@$"
+============================================
+|    Bem vindo ao sistema de cadastro      |
+|    de pessoa física e pessoa jurídica    |
+============================================
+");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            BarraCarregamento("Iniciando");
+            Console.ResetColor();
 
-            PessoaFisica novaPF = new PessoaFisica();
-            novaPF.CPF            = "12345678955";
-            novaPF.DataNascimento = new DateTime(1979, 11, 05);
-            novaPF.Nome           = "Rafael Ramos";
-            novaPF.Endereco       = endPF;
-            novaPF.Email          = "rlavinas@gmail.com";
-            novaPF.Salario        = 7000F;
-
-            Endereco endPJ = new Endereco();
-            endPF.Logradouro  = "Rua Silveira Martins";
-            endPF.Numero      = 24;
-            endPF.Complemento = "Apto 335";
-            endPF.EnderecoComercial = false;
-
-            PessoaJuridica novaPJ = new PessoaJuridica();
-            novaPJ.CNPJ        = "01001001000123";
-            novaPJ.RazaoSocial = "RRL";
-            novaPJ.Endereco    = endPJ;
-            novaPJ.Salario     = 20000F;
-
-            bool CNPJValido = novaPJ.ValidarCNPJ(novaPJ.CNPJ);
+            string cOpcao = "1";
             
-            Console.WriteLine(novaPJ.PagarImposto(novaPJ.Salario));
-            Console.WriteLine($"Imposto PJ: {novaPJ.PagarImposto(novaPJ.Salario)}");
+            while (cOpcao != "0")
+            {
+                Console.WriteLine(@$"
+========================================
+|    Escolha uma das opções abaixo     |
+|       1 - Pessoa Física              |
+|       2 - Pessoa Jurísica            |
+|       0 - Sair                       |
+========================================
+");  
+                Console.ResetColor();
+                cOpcao = Console.ReadLine();
+                switch (cOpcao)
+                {
+                    case "1":
+                        Endereco endPF = new Endereco();
+                        endPF.Logradouro  = "Rua Silveira Martins";
+                        endPF.Numero      = 24;
+                        endPF.Complemento = "Apto 113";
+                        endPF.EnderecoComercial = false;
 
-            if (CNPJValido == true){
-                Console.WriteLine("CNPJ válido");
-            } else {
-                Console.WriteLine("CNPJ inválido");
+                        PessoaFisica novaPF = new PessoaFisica();
+                        novaPF.CPF            = "12345678955";
+                        novaPF.DataNascimento = new DateTime(1979, 11, 05);
+                        novaPF.Nome           = "Rafael Ramos";
+                        novaPF.Endereco       = endPF;
+                        novaPF.Email          = "rlavinas@gmail.com";
+                        novaPF.Salario        = 7000F;
+
+                        Console.WriteLine($"Rua: {novaPF.Endereco.Logradouro}, {novaPF.Endereco.Numero}");
+                        Console.WriteLine($"Data Nascimento : {novaPF.DataNascimento}");
+                        Console.WriteLine($"Imposto PF: {novaPF.PagarImposto(novaPF.Salario)}");
+
+                        bool idadeValida = novaPF.ValidarDataNascimento(novaPF.DataNascimento);
+
+                        if (idadeValida == true) {
+                            System.Console.WriteLine($"Cadastro Aprovado");
+                        } else {
+                            System.Console.WriteLine($"Cadastro Reprovado");
+                        }
+
+                        Thread.Sleep(3000);
+
+                        break;
+                    case "2":
+                        Endereco endPJ = new Endereco();
+                        endPJ.Logradouro  = "Rua Silveira Martins";
+                        endPJ.Numero      = 24;
+                        endPJ.Complemento = "Apto 335";
+                        endPJ.EnderecoComercial = false;
+
+                        PessoaJuridica novaPJ = new PessoaJuridica();
+                        novaPJ.CNPJ        = "01001001000123";
+                        novaPJ.RazaoSocial = "RRL";
+                        novaPJ.Endereco    = endPJ;
+                        novaPJ.Salario     = 20000F;
+
+                        bool CNPJValido = novaPJ.ValidarCNPJ(novaPJ.CNPJ);
+                        
+                        Console.WriteLine(novaPJ.PagarImposto(novaPJ.Salario));
+                        Console.WriteLine($"Imposto PJ: {novaPJ.PagarImposto(novaPJ.Salario)}");
+
+                        if (CNPJValido == true){
+                            Console.WriteLine("CNPJ válido");
+                        } else {
+                            Console.WriteLine("CNPJ inválido");
+                        }
+
+                        Thread.Sleep(3000);
+
+                        break;
+                    case "0":
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Obrigado por utilizar nosso sistema");                        
+                        Console.WriteLine($"");
+                        BarraCarregamento("Finalizado");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine($"Opção inválida. Escolha uma opção válida.");
+                        Thread.Sleep(2000);
+                        break;
+                }
+                Console.Clear();
             }
 
-            Console.WriteLine($"Rua: {novaPF.Endereco.Logradouro}, {novaPF.Endereco.Numero}");
-            Console.WriteLine($"Data Nascimento : {novaPF.DataNascimento}");
-            Console.WriteLine($"Imposto PF: {novaPF.PagarImposto(novaPF.Salario)}");
-
-            bool idadeValida = novaPF.ValidarDataNascimento(novaPF.DataNascimento);
-
-            if (idadeValida == true) {
-                System.Console.WriteLine($"Cadastro Aprovado");
-            } else {
-                System.Console.WriteLine($"Cadastro Reprovado");
-            }
+            Console.ResetColor();
+        }
+        static void BarraCarregamento(string cTexto){
+            Console.Write($"{cTexto}");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(500);
+                Console.Write($".");     
+            } 
+            Console.WriteLine($""); 
         }
     }
 }
